052ffd4dcce6e95b68dfe47e004e51a417fa7983
Ari Bahtiar <aribahtiar@unsil.ac.id>
2019-12-06 11:01:18 +0700

    update:

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<script data-ad-client="ca-pub-3313481531647549" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -26,6 +27,7 @@
 
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  
   <style>
 
 .wa,.tel{
